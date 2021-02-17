7f0fa2149c91e4a591ad1e4dfc2658602dd34853
JimHin <krowstudio@icloud.com>
2021-02-17 20:58:22 +0100

    Création de la todolist dans Vuex, design de cette vue

---
+++ src/components/Todos/Todo.vue
@@ -0,0 +1,36 @@
+<template>
+    <q-item
+      @click="modifyTodo({id: todo.id, updates: {completed: !todo.completed}})"
+      clickable
+      v-ripple
+      :class="todo.completed ? 'bg-grey-4' : 'bg-grey-2'"
+    >
+      <q-item-section color="todo">
+        <q-item-label>{{ todo.title }}</q-item-label>
+      </q-item-section>
+      <q-item-section side top>
+        <q-checkbox color="secondary" :value="todo.completed" class="no-pointer-events"/>
+      </q-item-section>
+    </q-item>
+</template>
+
+<script>
+// importation des fonctions utilitaires
+import { mapActions } from 'vuex'
+export default {
+  name: 'Todo',
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    ...mapActions('todos', ['modifyTodo'])
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/TodoList.vue
+++ src/pages/TodoList.vue
@@ -1,71 +1,29 @@
 <template>
-<div class="q-pa-md q-gutter-sm">
-    <q-btn round color="secondary" icon="add" class="z-top add_button"/>
-    <q-list>
-    <q-item
-      v-for="todo in todos"
-      :key="todo.id"
-      @click="todo.completed = !todo.completed"
-      clickable
-      v-ripple
-    >
-      <q-item-section side top flex>
-        <q-item-label>{{ todo.title }}</q-item-label>
-        <q-checkbox v-model="todo.completed" />
-        <q-item-label caption lines="2">{{todo.content}}</q-item-label>
-      </q-item-section>
-    </q-item>
-    <q-separator spaced inset />
+  <div class="q-pa-md q-gutter-sm">
+    <q-list separator >
+      <todo v-for="todo in todos"
+             :key="todo.id"
+             :todo="todo">
+      </todo>
     </q-list>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'TodoList',
-  data () {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: 'Acheter des piles',
-          content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum repudiandae eveniet reprehenderit eos provident voluptatum, deleniti facilis asperiores in. Delectus repellat nulla rerum? Quasi sint facere esse. Asperiores, tenetur corrupti.',
-          completed: false,
-          createdAt: '06.01.2021',
-          completedAt: ''
-        },
-        {
-          id: 2,
-          title: 'Aller signer le contrat Owlie',
-          content: '',
-          completed: false,
-          createdAt: '17.01.2021',
-          completedAt: ''
-        },
-        {
-          id: 3,
-          title: 'Connecter l\'appli au firestore',
-          content: '',
-          completed: false,
-          createdAt: '06.02.2021',
-          completedAt: ''
-        },
-        {
-          id: 4,
-          title: 'conditionné accés à todolist au fait d\'être connecté',
-          content: '',
-          completed: false,
-          createdAt: '06.02.2021',
-          completedAt: ''
-        }
-      ]
-    }
-  }
+  components: {
+    Todo: require('components/Todos/Todo').default
+  },
+  computed: {
+    ...mapGetters('todos', ['todos'])
+  },
+  methods: {
 
+  }
 }
 </script>
 <style lang="scss" scoped>
-  .add_button{
-    margin-top: -2em;
-  }
+
 </style>
