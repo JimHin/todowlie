453f25c3965085caacbf5cf42241b8bd5d04ca33
JimHin <krowstudio@icloud.com>
2021-02-18 13:52:50 +0100

    Placement du header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -19,26 +19,11 @@
                         <q-icon name="search" class="search_icon"/>
                 </div>
             </div>
-
             <div class="row">
-                <div class="col">
-                    <div v-if="this.$route.fullPath === '/todolist'">
-                        <q-btn round color="secondary" v-if="!newTodoOpen" icon="add" class="add_button" @click="newTodoOpen = !newTodoOpen"/>
-                    </div>
-                </div>
-                <div class="col-6">
-                     <q-toolbar-title class="title" v-if="!newTodoOpen">
-                        Owlie Todolist
-                    </q-toolbar-title>
+                <div class="col" id="addButton">
+                    <form-todo />
                 </div>
                 <div class="col-3">
-                    <div v-if="this.$route.fullPath === '/todolist'">
-                        <q-input v-if="newTodoOpen" label-color="teal" dark filled borderless focused label="Ajouter un élément" class="newtodo" id="task">
-                                <q-avatar>
-                                    <q-icon ><q-btn round color="secondary" v-if="newTodoOpen" icon="add" class="add_button_input" @click="newTodoOpen = !newTodoOpen"/></q-icon>
-                                </q-avatar>
-                        </q-input>
-                    </div>
                 </div>
             </div>
 
@@ -86,28 +71,19 @@
   .search_icon{
     font-size: 2em;
   }
-  .add_button{
-    margin-top: 5em;
-  }
-  /* input newtodo style */
-  .newtodo{
-      margin-top: 3em;
-      margin-left: -10em;
-  }
-
 </style>
 <script>
 import AuthenticateIndex from 'pages/Authenticate'
-
+import FormTodo from 'src/components/Todos/FormTodo.vue'
 export default {
   name: 'MainLayout',
   components: {
-    AuthenticateIndex
+    AuthenticateIndex,
+    FormTodo
   },
   data () {
     return {
-      leftDrawerOpen: false,
-      newTodoOpen: false
+      leftDrawerOpen: false
     }
   },
   methods: {
--- src/pages/TodoList.vue
+++ src/pages/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="q-pa-md q-gutter-sm">
     <q-list separator >
-      <q-label v-if="todos.length === 0" >Aucun élément</q-label>
+      <q-label v-if="todos.length === 0" name="emptyList">Aucun élément</q-label>
       <todo v-for="todo in todos"
              :key="todo.id"
              :todo="todo">
@@ -12,13 +12,14 @@
 
 <script>
 import { mapGetters } from 'vuex'
+
 export default {
   name: 'TodoList',
   components: {
     Todo: require('components/Todos/Todo').default
   },
   computed: {
-    ...mapGetters('todos', ['todos'])
+    ...mapGetters('todos', ['todos'], 'newTodoOPen', ['newTodoOPen'])
   },
   methods: {
 
