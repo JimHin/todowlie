01a7031feca01abc1d5802bf52778e40a1dc617f
Jean-michel Hinicker <36866750+JimHin@users.noreply.github.com>
2021-02-19 17:57:17 +0100

    Update firebase.js

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -2,13 +2,13 @@ import firebaseService from '../services/firebase'
 
 export default async () => {
   const config = {
-    apiKey: 'AIzaSyDHvx1XPPgLVhdda2cr9kJN5lJrK1wwtck',
-    authDomain: 'todowlie.firebaseapp.com',
-    projectId: 'todowlie',
-    storageBucket: 'todowlie.appspot.com',
-    messagingSenderId: '583074680185',
-    appId: '1:583074680185:web:1bbfa6a4a8228ca3cf33a8',
-    measurementId: 'G-1S78PM8NJW'
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
   }
   firebaseService.fBInit(config)
 
