266d71fb46fe1543f6715bfea4d117354e9d0341
JimHin <krowstudio@icloud.com>
2021-02-21 01:13:44 +0100

    création du store auth

--- src/components/Todos/Todo.vue
+++ src/components/Todos/Todo.vue
@@ -15,7 +15,12 @@
           </q-item-section>
           </q-item>
           <div v-if="toggleDetail">
-            <textarea v-model="todo.detail" class="detail" id="editable"></textarea>
+            <q-form
+            @submit="formSubmit"
+            >
+              <input type="textarea" :value="todo.detail" class="detail" id="editable"/>
+              <input type="submit" value="modifier">
+            </q-form>
            <!--  <q-icon
               name="create"
               class="create_icon"
@@ -54,8 +59,8 @@ export default {
   },
   data () {
     return {
-      toggleDetail: false,
-      displayFormDetail: false
+      toggleDetail: false
+      // displayFormDetail: false
     }
   },
   props: {
@@ -65,11 +70,32 @@ export default {
     }
   },
   methods: {
-    ...mapActions('todos', ['removeTodo'])
+    ...mapActions('todos', ['removeTodo']),
+    ...mapActions('todos', ['modifyTodo']),
+    formSubmit () {
+      console.log('submit')
+      if (this.todo.id) {
+        console.log('submit-if-todo')
+        // Construction du payload
+        const payload = {
+          id: this.todo.id,
+          detail: this.todo.detail,
+          updates: this.todo // Passe toutes les propriétés de la todo actuelle
+        }
+        console.log(payload.detail)
+        // Appel l'action modifyTodo et lui passe le payload
+        this.modifyTodo(payload)
+      }
+    },
+    mounted () {
+      if (this.todoToUpdate) {
+        // Copie les propriétés de tacheAModifier dans un nouvel objet vide
+        this.todo = Object.assign({}, this.todoToUpdate)
+      }
+    }
   }
 }
 </script>
-
 <style>
   .detail{
     width: 100%;
