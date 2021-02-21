f9fe8ffce41023ad474faf85fa804c61870fbaee
Jean-michel Hinicker <36866750+JimHin@users.noreply.github.com>
2021-02-21 20:33:18 +0100

    Update store-auth.js

--- src/store/store-auth.js
+++ src/store/store-auth.js
@@ -1,37 +1,72 @@
-// State : données du magasin
+import { Loading } from 'quasar'
+import { firebaseAuth } from 'src/boot/firebase'
+
 const state = {
+  loggedIn: false
 }
 
-/*
-Mutations : méthode qui manipulent les données
-Les mutations ne peuvent pas être asynchrones !!!
-*/
 const mutations = {
-
+  setLoggedIn (state, value) {
+    state.loggedIn = value
+  }
 }
 
-/*
-Actions : méthodes du magasin qui font appel aux mutations
-Elles peuvent être asynchrones !
-*/
 const actions = {
+  // action permettant l'enregistrement d'un utilisateur
+  registerUser ({ user }, payload) {
+    console.log('registerUser: ', payload)
+    // méthode d'enregistrement d'un utilisateur'
+    firebaseAuth.createUserWithEmailAndPassword(payload.email, payload.password)
+      .then(response => {
+        console.log('response', response)
+        this.setLoggedIn(state, true)
+      })
+      .catch(error => {
+        console.log('error.message', error.message)
+      })
+  },
+  // action permettant la connexion d'un utilisateur
+  loginUser ({ user }, payload) {
+    // méthode de connexion d'un utilisateur
+    firebaseAuth.signInWithEmailAndPassword(payload.email, payload.password)
+      .then(response => {
+        console.log('response', response)
+        console.log(this.loggedIn)
+      })
+      .catch(error => {
+        console.log('error.message', error.message)
+      })
+  },
+  // action permettant la déconnexion d'un utilisateur
+  logoutUser () {
+    // méthode de déconnexion de firebase
+    firebaseAuth.signOut()
+  },
+  // action permettant de vérifier l'état du state loggedIn d'un utilisateur
+  handleAuthStateChange ({ commit }) {
+    console.log('handleAuthStateChange')
+    // méthode catchant le changement d'état d'un utilisateur
+    firebaseAuth.onAuthStateChanged((user) => {
+      Loading.hide()
+      if (user) {
+        // L'utilisateur est connecté
+        commit('setLoggedIn', true)
+        console.log('utilisateur connecté')
+        this.$router.push('todolist')
+      } else {
+        // L'utilisateur n'est pas connecté
+        commit('setLoggedIn', false)
+        console.log('utilisateur non connecté')
+        this.$router.replace('/')
+      }
+    })
+  }
 }
 
-/*
-Getters : retourne les données du magasin
-Fonctionne comme les propriétés calculées
-Sert à calculer, trier, filtrer ou formater les donneés
-*/
 const getters = {
 
 }
 
-/*
-Exporte les constantes, variables du fichier
-On pourra ainsi les récupérer, les importer dans un autre fichier JS.
-
-namespace: true, ajoute un namespace l'objet retourné.
-*/
 export default {
   namespaced: true,
   state,
