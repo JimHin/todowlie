aaffa6063216477dbf4ebe3a75dd94c7e19a89a8
Jean-michel Hinicker <36866750+JimHin@users.noreply.github.com>
2021-02-21 20:17:05 +0100

    Update firebase.js

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,21 +1,20 @@
 import firebaseService from '../services/firebase'
 import 'firebase/auth'
-import 'firebase/firestore'
 
-export default async () => {
-  const config = {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: ''
-  }
-  firebaseService.fBInit(config)
-
-  // Validation that our service structure is working
-  // with a firebase app instance. Does not validate a
-  // valid API key.
-  console.log(firebaseService.auth())
+const config = {
+  apiKey: 'AIzaSyDHvx1XPPgLVhdda2cr9kJN5lJrK1wwtck',
+  authDomain: 'todowlie.firebaseapp.com',
+  projectId: 'todowlie',
+  storageBucket: 'todowlie.appspot.com',
+  messagingSenderId: '583074680185',
+  appId: '1:583074680185:web:1bbfa6a4a8228ca3cf33a8',
+  measurementId: 'G-1S78PM8NJW'
 }
+const firebaseApp = firebaseService.fBInit(config)
+
+// Validation that our service structure is working
+// with a firebase app instance. Does not validate a
+// valid API key.
+console.log(firebaseService.auth())
+const firebaseAuth = firebaseApp.auth()
+export { firebaseAuth }
